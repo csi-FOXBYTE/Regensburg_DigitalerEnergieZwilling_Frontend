77ab1e8484f740a1441aad6fb7b3df08bfe97c8e
Nico Vogel <Nico.Vogel@csi-online.de>
2026-08-28 17:48:09 +0200

    refactor: configure footer partner logos

---
+++ src/config/footerPartners.ts
@@ -0,0 +1,18 @@
+export const footerPartners = [
+  {
+    src: '/assets/R_NEXT_Logo_dark.svg',
+    alt: 'R_NEXT',
+  },
+  {
+    src: '/assets/KfW_RGB_neg.svg',
+    alt: 'KfW',
+  },
+  {
+    src: '/assets/RGpub_dark_grey.svg',
+    alt: 'Regensburg plant und baut',
+  },
+  {
+    src: '/assets/250317_BMWSB_quadratisch_sw.png',
+    alt: 'Gefördert durch das Bundesministerium für Wohnen, Stadtentwicklung und Bauwesen',
+  },
+] as const;
