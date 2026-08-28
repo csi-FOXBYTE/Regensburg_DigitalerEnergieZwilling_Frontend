813dc53459eb9d671570118d35bd94c98c73848b
Nico Vogel <Nico.Vogel@csi-online.de>
2026-08-28 17:45:08 +0200

    refactor: centralize municipal map geography

---
+++ src/config/map.ts
@@ -0,0 +1,16 @@
+export const mapConfig = {
+  initialView: {
+    longitudeDegrees: 12.09303665836369,
+    latitudeDegrees: 49.02157200002277,
+    heightMeters: 502.1862266683654,
+    headingDegrees: 123.36360472086916,
+    pitchDegrees: -18.459972601788056,
+    rollDegrees: 359.99982576214444,
+  },
+  sessionTargetBounds: {
+    westDegrees: 11.5,
+    southDegrees: 48.5,
+    eastDegrees: 13,
+    northDegrees: 49.5,
+  },
+} as const;
